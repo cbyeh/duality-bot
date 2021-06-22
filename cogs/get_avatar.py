1b9d39ce63421975084bbe3fe24c14b9f3f8c57c
Cy <chrisbcyeh@gmail.com>
2021-06-21 20:38:49 -0700

    Avatar working in Guild

--- cogs/get_avatar.py
+++ cogs/get_avatar.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 
 
 class GetAvatar(commands.Cog):
@@ -7,7 +8,20 @@ class GetAvatar(commands.Cog):
 
     @commands.command(aliases=["getavatar"])
     async def avatar(self, message):
-        await message.channel.send("Getting avatar...")
+        tokens = message.message.content.split()
+        if len(tokens) > 2:
+            return await message.reply(
+                "Invalid usage.\n```$avatar\n$avatar ExampleUser#1234```"
+            )
+        if len(tokens) == 1:
+            return await message.reply(message.message.author.avatar_url)
+        # user = await self.bot.fetch_user(tokens[1])
+        converter = commands.MemberConverter()
+        try:
+            member = await converter.convert(message, tokens[1])
+            await message.reply(member.avatar_url)
+        except discord.ext.commands.errors.MemberNotFound:
+            return await message.reply("User not found")
 
 
 def setup(bot):
